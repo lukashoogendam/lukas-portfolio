d4c1f3d70cc90cbf717f578e84dd3f3496d0fef9
s1142688 <s1142688@student.hsleiden.nl>
2026-05-12 09:48:36 +0200

    initial commit backend

---
+++ backend/src/main/java/nl/lukas/portfolio/PortfolioApplication.java
@@ -0,0 +1,17 @@
+package nl.lukas.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan("nl.lukas.portfolio.config")
+public class PortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioApplication.class, args);
+	}
+
+}
+
+
